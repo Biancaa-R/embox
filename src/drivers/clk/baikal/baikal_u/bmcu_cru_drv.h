57d74e5718decce75851a91196647d13dac0d9f5
Anton Bondarev <anton.bondarev2310@gmail.com>
2025-10-10 19:18:47 +0300

    drivers: Work on drivers/clk/baikal/baikal_u

---
+++ src/drivers/clk/baikal/baikal_u/bmcu_cru_drv.h
@@ -0,0 +1,14 @@
+/**
+ * @file
+ *
+ * @author Anton Bondarev
+ *
+ * @date 10.10.2025
+ */
+
+#ifndef DRIVERS_CLK_BAIKAL_BMCU_CRU_H
+#define DRIVERS_CLK_BAIKAL_BMCU_CRU_H
+
+extern int clk_enable(char *clk_name);
+
+#endif /* DRIVERS_CLK_BAIKAL_BMCU_CRU_H */
